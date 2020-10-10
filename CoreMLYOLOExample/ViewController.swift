cec4a68dccbf60b443bc55ac3058cf4fd461bba4
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-10-11 05:06:35 +0900

    first commit

---
+++ CoreMLYOLOExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLYOLOExample
+//
+//  Created by TokyoYoshida on 2020/10/11.
+//  Copyright © 2020 TokyoMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
